9244f5d95fbe899b4a74861301b9344c64ae11c2
ganah <dinggan@whu.edu.cn>
2021-06-26 19:53:51 +0800

    <add> Added VINS_Fusion_RGBD VIO for uavNavigation

---
+++ Solution/uav_navigation/VINS-Fusion-RGBD/scripts/run_realsense_rgbd_imu.sh
@@ -0,0 +1,1 @@
+rosrun vins vins_node /home/ganahe/catkin_ws/src/VINS-Fusion-RGBD/config/realsense/realsense_depth_imu_config.yaml
--- Solution/uav_navigation/experiment_fast_planner/scripts/run_vins_fusion-node.sh
+++
@@ -1,2 +0,0 @@
-
-rosrun vins vins_node ~/catkin_ws/src/VINS-Fusion/config/realsense/realsense_stereo_imu_config.yaml
