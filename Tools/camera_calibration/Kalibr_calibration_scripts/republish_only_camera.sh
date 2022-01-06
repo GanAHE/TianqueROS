e10d3f7646b48321177004febb3b299f2387e663
ganah <dinggan@whu.edu.cn>
2022-01-06 21:37:59 +0800

    <update>Improve project structure

--- Tools/camera_calibration/Kalibr_calibration_scripts/republish_only_camera.sh
+++ Tools/camera_calibration/Kalibr_calibration_scripts/republish_only_camera.sh
@@ -2,16 +2,16 @@
 
 echo "------ GanAHE Republish Topic -------"
 
-#rosrun topic_tools throttle messages /camera/color/image_raw 4.0 /rgb_camera &
+rosrun topic_tools throttle messages /camera/color/image_raw 4.0 /rgb_camera &
 
-rosrun topic_tools throttle messages /camera/infra1/image_rect_raw 4.0 /infra_left &
+#rosrun topic_tools throttle messages /camera/fisheye1/image_raw 4.0 /infra_left &
 
-rosrun topic_tools throttle messages /camera/infra2/image_rect_raw 4.0 /infra_right &
+#rosrun topic_tools throttle messages /camera/fisheye2/image_raw 4.0 /infra_right &
 
 sleep 1s
 red="n"
 read -p "record now? y/n>> " red
 echo "--- $red"
 if [ "$red" == "y" ];then
-    rosbag record -O stereo_640_480.bag /infra_left /infra_right
+    rosbag record -O rgb_1280_720.bag /infra_left /infra_right /rgb_camera
 fi
